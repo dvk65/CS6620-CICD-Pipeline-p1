cda138512d4a34eb48266c21d04fa63b9005da52
Evangeline Kim <kim.eva@northeastern.edu>
2025-05-15 21:57:32 -0400

    Added first draft workflow and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # CS6620-CICD-Pipeline-p1
-A simple Pokémon-themed Python module to show unit testing/automation with GitHub Workflows. HW assignment for CS6620 (Cloud Computing) at the Roux Institute at NEU
+This is a simple Pokémon-themed Python module to show unit testing/automation with GitHub Workflows. HW assignment for CS6620 (Cloud Computing) at the Roux Institute at Northeastern.
+
+## Continuous Integration
+This project makes use of GitHub Actions to to automatically run tests whenever code is pushed to the main repo, a pull request is opened, or the workflow is manually triggered on GitHub. Test workflow can be triggered manually by going to the `Actions` tab and clicking on the `Run Workflow` button under the Pokemon Tests workflow. You can view the configuration in `workflows/main.yml`.
+
+## Setup
+You can run this project using either `pip` or `conda`. First make sure to properly clone and enter the repository.
+
+### Using `pip`:
+```bash
+# Create/activate venv (optional)
+python3 -m venv venv
+source venv/bin/activate
+# Windows: venv\Scripts\activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run tests
+PYTHONPATH=src pytest tests/
+```
+
+### Using `conda`:
+```bash
+# Create/activate the environment
+conda env create -f environment.yml
+conda activate cicd_pipeline_p1_env
+
+# Run tests
+PYTHONPATH=src pytest tests/
+# Windows: set PYTHONPATH=src && pytest tests/
+```
--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -4,28 +4,38 @@ This is the test script for the pokemon.py. Will use simple tests such as testin
 from pokemon import Pokemon
 
 def test_taking_damage():
-    charmander = Pokemon("Charmander", 100) # creating a pokemon
-    charmander.take_damage(99) # taking 50 damage
-    assert charmander.hp == 1 # hp should be at 1 after taking 99 damage
+    '''Creating a pokemon, taking damage, and seeing if its the correct number. I'm going simple with my choices of pkmn.
+    '''
+    charmander = Pokemon("Charmander", 100)
+    charmander.take_damage(99)
+    assert charmander.hp == 1
 
-def test_faining():
-    bulbasaur = Pokemon("Bulbasaur", 100) # another pkmn, (I'm not being very creative here)
-    bulbasaur.take_damage(100) # taking 100 damage
-    assert bulbasaur.hp == 0 # hp will hit 0...
-    assert bulbasaur.fainted == True # ...so he should be knocked out
+def test_fainting():
+    ''' Bulbasaur's turn. Pokemon should faint if their hp hits 0
+    '''
+    bulbasaur = Pokemon("Bulbasaur", 100)
+    bulbasaur.take_damage(100)
+    assert bulbasaur.hp == 0
+    assert bulbasaur.fainted == True
 
 def test_healing():
-    squirtle = Pokemon("Squirtle", 100) # I'm just going through the starters, lol
-    squirtle.take_damage(50) # taking 50 damage
-    squirtle.heal(20) # healing 20 (used a potion!)
-    assert squirtle.hp == 70 # taking 50 damage and healing 20 shoudl be at 70
+    '''Now it's Squirtle's turn. Time to see if healing gives the right hp.
+    '''
+    squirtle = Pokemon("Squirtle", 100)
+    squirtle.take_damage(50)
+    squirtle.heal(20)
+    assert squirtle.hp == 70
 
 def test_healing_over():
+    '''Also need to make sure that healing doesn't go over the max hp of the pkmn
+    '''
     squirtle = Pokemon("Squirtle", 100)
-    squirtle.heal(20) # Should have hp of 100 instead of going over to 120
+    squirtle.heal(20)
     assert squirtle.hp == 100
 
 def test_attack():
-    absol = Pokemon("Absol", 100) # why absol? because I think he's cool :p
+    '''Testing an attack. Why Absol you say? Because I think he's cool :p
+    '''
+    absol = Pokemon("Absol", 100) # i love absol <3
     attack = absol.attack("Night Slash", 85)
     assert attack == "Absol used Night Slash. It caused 85 damage!"
